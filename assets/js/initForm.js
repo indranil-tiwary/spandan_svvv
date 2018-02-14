b15269e31544a48f5bed3883d9f6350f4bdecec5
Indranil Tiwary <indranil-tiwary@users.noreply.github.com>
2018-02-14 14:20:16 +0530

    form final

--- assets/js/initForm.js
+++ assets/js/initForm.js
@@ -210,7 +210,6 @@ jQuery( document ).ready(function( $ ) {
 			document.getElementById('branch').value,
 			document.getElementById('degree').value,
 		);
-		window.location.href = "dashboard.html";
 	});
 
 
